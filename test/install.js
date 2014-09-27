b3030d26a7e8513a3dc87d3fc89e429ab747a7ce
ajlopez <webmaster@ajlopez.com>
2014-09-27 17:12:49 -0300

    fix install test

--- test/install.js
+++ test/install.js
@@ -13,10 +13,10 @@ exports['Install module'] = function (test) {
     
     hello.install(ajgenesis, function (err, data) {
         test.ok(!err);
-        fs.existsSync('ajgenesis');
-        fs.existsSync(path.join('ajgenesis', 'modules'));
-        fs.existsSync(path.join('ajgenesis', 'modules', 'hello'));
-        fs.existsSync(path.join('ajgenesis', 'modules', 'hello', 'readme.txt'));
+        test.ok(fs.existsSync('ajgenesis'));
+        test.ok(fs.existsSync(path.join('ajgenesis', 'modules')));
+        test.ok(fs.existsSync(path.join('ajgenesis', 'modules', 'hello')));
+        test.ok(fs.existsSync(path.join('ajgenesis', 'modules', 'hello', 'readme.txt')));
         process.chdir(cwd);
         test.done();
     });
