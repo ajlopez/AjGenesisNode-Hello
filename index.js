f815c4ca0d07ab128efc90c7f929c6d98b12a03c
ajlopez <webmaster@ajlopez.com>
2015-12-21 10:49:01 -0300

    Using ajgenesis.fs

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 var path = require('path');
 
 function install(ajgenesis, cb) {
-    ajgenesis.createDirectory('ajgenesis');
-    ajgenesis.createDirectory(path.join('ajgenesis', 'modules'));
-    ajgenesis.createDirectory(path.join('ajgenesis', 'modules', 'hello'));
-    ajgenesis.copyDirectory(path.join(__dirname, 'module'), path.join('ajgenesis', 'modules', 'hello'), cb);
+    ajgenesis.fs.createDirectory('ajgenesis');
+    ajgenesis.fs.createDirectory(path.join('ajgenesis', 'modules'));
+    ajgenesis.fs.createDirectory(path.join('ajgenesis', 'modules', 'hello'));
+    ajgenesis.fs.copyDirectory(path.join(__dirname, 'module'), path.join('ajgenesis', 'modules', 'hello'), cb);
 }
 
 module.exports = {
